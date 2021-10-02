268c5bed1eee010db1665ed18911e4f7e7584094
HabiburRahman1 <habib.usa2014@gmail.com>
2021-10-02 15:51:55 +0600

    View Updated

--- models/wf_employee.py
+++ models/wf_employee.py
@@ -41,16 +41,3 @@ class WorkfolioEmployee(models.Model):
 
     employee_id = fields.Many2one('hr.employee', string='Responsible Person')
     user_id = fields.Many2one('res.users', string='Responsible User')
-
-
-
-
-
-    # total_worked_days = fields.Char(string="Total Worked Days")
-    # total_worked_second = fields.Char(string="Total Worked Second")
-    # total_productive_second = fields.Char(string="Total Productive Second")
-    # total_unproductive_second = fields.Char(string="Total Unproductive Second")
-    # total_neutral_second = fields.Char(string="Total Neutral Second")
-    # total_idle_second = fields.Char(string="Total Idle Second")
-    # total_break_second = fields.Char(string="Total Break Second")
-    # total_active_second = fields.Char(string="Total Active Second")
